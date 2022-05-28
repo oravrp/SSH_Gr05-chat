c87f8c71bbfa42a851b59f9ef9d72027478e3e84
unknown <blerina.haziri7@student.uni-pr.edu>
2022-05-28 14:54:05 +0200

    Login of chat client

---
+++ P2P/ChatClient/src/com/muc/ChatClient.java
@@ -0,0 +1,180 @@
+package com.muc;
+
+import org.apache.commons.lang3.StringUtils;
+
+import java.io.*;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class ChatClient {
+    private final String serverName;
+    private final int serverPort;
+    private Socket socket;
+    private InputStream serverIn;
+    private OutputStream serverOut;
+    private BufferedReader bufferedIn;
+
+    private ArrayList<UserStatusListener> userStatusListeners = new ArrayList<>();
+    private ArrayList<MessageListener> messageListeners = new ArrayList<>();
+
+
+    //Kontruksori i klases ChatClient
+    public ChatClient(String serverName, int serverPort) {
+        this.serverName = serverName;
+        this.serverPort = serverPort;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ChatClient client = new ChatClient("localhost", 8818); //create an instance of this client. The host is localhost
+        client.addUserStatusListener(new UserStatusListener() {
+            @Override
+            public void online(String login) {
+                System.out.println("ONLINE: " + login);
+            }
+
+            @Override
+            public void offline(String login) {
+                System.out.println("OFFLINE: " + login);
+            }
+        });
+
+        client.addMessageListener(new MessageListener() {
+            @Override
+            public void onMessage(String fromLogin, String msgBody) {
+                System.out.println("You got a message from " + fromLogin + " ===>" + msgBody);
+            }
+        });
+
+        if (!client.connect()) { //nese nuk u realizu connection me server
+            System.err.println("Connect failed.");
+        } else {
+            System.out.println("Connect successful"); //if the connection is succeful
+
+            if (client.login("guest", "guest")) { //we want to login
+                System.out.println("Login successful");
+
+                client.msg("jim", "Hello World!");
+            } else {
+                System.err.println("Login failed");
+            }
+
+            //client.logoff();
+        }
+    }
+
+    public void msg(String sendTo, String msgBody) throws IOException {
+        String cmd = "msg " + sendTo + " " + msgBody + "\n";
+        serverOut.write(cmd.getBytes());
+    }
+
+    public boolean login(String login, String password) throws IOException {
+        String cmd = "login " + login + " " + password + "\n";
+        serverOut.write(cmd.getBytes());
+
+        String response = bufferedIn.readLine();
+        System.out.println("Response Line:" + response);
+
+        if ("ok login".equalsIgnoreCase(response)) {
+            startMessageReader();
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public void logoff() throws IOException {
+        String cmd = "logoff\n";
+        serverOut.write(cmd.getBytes());
+    }
+
+    private void startMessageReader() {
+        Thread t = new Thread() {
+            @Override
+            public void run() {
+                readMessageLoop();
+            }
+        };
+        t.start();
+    }
+
+    private void readMessageLoop() {
+        try {
+            String line;
+            while ((line = bufferedIn.readLine()) != null) {
+                String[] tokens = StringUtils.split(line);
+                if (tokens != null && tokens.length > 0) {
+                    String cmd = tokens[0];
+                    if ("online".equalsIgnoreCase(cmd)) {
+                        handleOnline(tokens);
+                    } else if ("offline".equalsIgnoreCase(cmd)) {
+                        handleOffline(tokens);
+                    } else if ("msg".equalsIgnoreCase(cmd)) {
+                        String[] tokensMsg = StringUtils.split(line, null, 3);
+                        handleMessage(tokensMsg);
+                    }
+                }
+            }
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            try {
+                socket.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void handleMessage(String[] tokensMsg) {
+        String login = tokensMsg[1];
+        String msgBody = tokensMsg[2];
+
+        for(MessageListener listener : messageListeners) {
+            listener.onMessage(login, msgBody);
+        }
+    }
+
+    private void handleOffline(String[] tokens) {
+        String login = tokens[1];
+        for(UserStatusListener listener : userStatusListeners) {
+            listener.offline(login);
+        }
+    }
+
+    private void handleOnline(String[] tokens) {
+        String login = tokens[1];
+        for(UserStatusListener listener : userStatusListeners) {
+            listener.online(login);
+        }
+    }
+
+    public boolean connect() { //qe per me realizu connection me server duhemi me kriju socket
+        try {
+            this.socket = new Socket(serverName, serverPort); //host and port. This.socket qe me mujt me ju qas edhe prej pjesve tjera t'programit
+            System.out.println("Client port is " + socket.getLocalPort()); //We can figure our which port this socket client is using
+            this.serverOut = socket.getOutputStream(); //
+            this.serverIn = socket.getInputStream(); //
+            this.bufferedIn = new BufferedReader(new InputStreamReader(serverIn));
+            return true;
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public void addUserStatusListener(UserStatusListener listener) {
+        userStatusListeners.add(listener);
+    }
+
+    public void removeUserStatusListener(UserStatusListener listener) {
+        userStatusListeners.remove(listener);
+    }
+
+    public void addMessageListener(MessageListener listener) {
+        messageListeners.add(listener);
+    }
+
+    public void removeMessageListener(MessageListener listener) {
+        messageListeners.remove(listener);
+    }
+
+}
