81137f1f11e85562a1eb2288dac91cce2deace18
unknown <blerina.haziri7@student.uni-pr.edu>
2022-05-29 22:43:55 +0200

    changes on main class

---
+++ P2P/Client/src/Main.java
@@ -0,0 +1,14 @@
+import messengerclient.ClientManager;
+import messengerclient.LoginFrame;
+
+public class Main {
+
+
+    public static void main(String[] args) 
+    {
+        ClientManager clientManager=new ClientManager();
+        LoginFrame loginFrame=new LoginFrame(clientManager);
+        loginFrame.setLocation(400,100);
+        loginFrame.setVisible(true);
+    }
+}
